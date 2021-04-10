da24b1314458c0bb0b115e127201b891c12cecbd
AlanConcepcion <60986431+AlanConcepcion@users.noreply.github.com>
2021-04-10 17:54:30 -0400

    se habilito el nombre de la cuenta.

--- Cuentas.Designer.cs
+++ Cuentas.Designer.cs
@@ -71,7 +71,6 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Enabled = false;
             this.label2.FlatStyle = System.Windows.Forms.FlatStyle.System;
             this.label2.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label2.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
